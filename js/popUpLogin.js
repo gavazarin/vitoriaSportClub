3202542f356633f96893f7ccc8559cf7cde1b04a
gavazarin <gavazarin@gmail.com>
2020-09-29 11:50:45 +0100

    ajustes - modal login + section novo nº sócio

--- js/popUpLogin.js
+++ js/popUpLogin.js
@@ -4,12 +4,8 @@ function iniciaModal(modalID) {
     // $('body').addClass('noScroll')
 }
 const btn = document.querySelector('.btn-login');
-btn.addEventListener('click', function() {
+btn.addEventListener('click', function () {
     iniciaModal('modalLogin');
-})
-const btnClubesFan = document.querySelector('.open-clubes-fan');
-btnClubesFan.addEventListener('click', function() {
-    iniciaModal('modalClubesFan');
 })
 
 
@@ -19,10 +15,6 @@ function fechaModal(modalID) {
     // $('body').removeClass('noScroll')
 }
 const close = document.querySelector('.closeModal');
-close.addEventListener('click', function() {
+close.addEventListener('click', function () {
     fechaModal('modalLogin');
-})
-const closeClubesFan = document.querySelector('.close-clubes-fan');
-closeClubesFan.addEventListener('click', function() {
-    fechaModal('modalClubesFan');
 })
